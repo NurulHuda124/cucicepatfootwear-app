5c3acae215edfa9ab908fb5072a78ad851d634e6
NurulHuda124 <rulhuda313@gmail.com>
2023-05-07 13:51:59 +0700

    ubah warna

--- resources/views/components/dashboard/sidebar.blade.php
+++ resources/views/components/dashboard/sidebar.blade.php
@@ -1,4 +1,4 @@
-<aside class="main-sidebar sidebar-dark-primary elevation-4">
+<aside class="main-sidebar sidebar-dark-warning elevation-4">
     <!-- Brand Logo -->
     <a href="/dashboard" class="brand-link">
         <img src="/img/logo2.png" alt="AdminLTE Logo" class="brand-image" style="opacity: 1" sizes="80x90">
